5cd32ac8ef30e994ec6ab2cfa25e779c98adddf8
dsimp7 <111376136+dsimp7@users.noreply.github.com>
2022-11-21 21:02:07 -0500

    updated bitcointracker.py
    updated the file to the correct beautified style

--- bitcointracker.py
+++ bitcointracker.py
@@ -4,7 +4,6 @@ load("encoding/base64.star", "base64")
 
 COINDESK_PRICE_URL = "https://api.coindesk.com/v1/bpi/currentprice.json"
 
-# Load Bitcoin icon from base64 encoded data
 BTC_ICON = base64.decode("""
 iVBORw0KGgoAAAANSUhEUgAAABEAAAARCAYAAAA7bUf6AAAAlklEQVQ4T2NkwAH+H2T/jy7FaP+
 TEZtyDEG4Zi0TTPXXzoDF0A1DMQRsADbN6MZdO4NiENwQbAbERh1lWLzMmgFGo5iFZBDYEFwuwG
@@ -15,15 +14,20 @@ AYozjsoBoE45OZi5DRBSnkCAMLhlPBiQGHlAAAAAElFTkSuQmCC
 def main():
     rep = http.get(COINDESK_PRICE_URL)
     if rep.status_code != 200:
-        fail("CoinDesk request failed with status %d", rep.status_code)
+        fail("Coindesk request failed with status %d", rep.status_code)
 
     rate = rep.json()["bpi"]["USD"]["rate_float"]
 
     return render.Root(
-        child = render.Row( # Row lays out its children horizontally
+        child = render.Box( # This Box exists to provide vertical centering
+            render.Row(
+                expanded=True, # Use as much horizontal space as possible
+                main_align="space_evenly", # Controls horizontal alignment
+                cross_align="center", # Controls vertical alignment
                 children = [
                     render.Image(src=BTC_ICON),
                     render.Text("$%d" % rate),
                 ],
-        )
+            ),
+        ),
     )
