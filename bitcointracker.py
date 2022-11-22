ba948621ccebbb4139618ed7841885effe148f32
dsimp7 <111376136+dsimp7@users.noreply.github.com>
2022-11-21 21:03:01 -0500

    update bitcointracker.py
    added the anti-spam to prevent unnecessary requests of cache to coindesk.

--- bitcointracker.py
+++ bitcointracker.py
@@ -1,6 +1,7 @@
 load("render.star", "render")
 load("http.star", "http")
 load("encoding/base64.star", "base64")
+load("cache.star", "cache")
 
 COINDESK_PRICE_URL = "https://api.coindesk.com/v1/bpi/currentprice.json"
 
@@ -12,18 +13,24 @@ AYozjsoBoE45OZi5DRBSnkCAMLhlPBiQGHlAAAAAElFTkSuQmCC
 """)
 
 def main():
-    rep = http.get(COINDESK_PRICE_URL)
-    if rep.status_code != 200:
-        fail("Coindesk request failed with status %d", rep.status_code)
-
-    rate = rep.json()["bpi"]["USD"]["rate_float"]
+    rate_cached = cache.get("btc_rate")
+    if rate_cached != None:
+        print("Hit! Displaying cached data.")
+        rate = int(rate_cached)
+    else:
+        print("Miss! Calling CoinDesk API.")
+        rep = http.get(COINDESK_PRICE_URL)
+        if rep.status_code != 200:
+            fail("Coindesk request failed with status %d", rep.status_code)
+        rate = rep.json()["bpi"]["USD"]["rate_float"]
+        cache.set("btc_rate", str(int(rate)), ttl_seconds=240)
 
     return render.Root(
-        child = render.Box( # This Box exists to provide vertical centering
+        child = render.Box(
             render.Row(
-                expanded=True, # Use as much horizontal space as possible
-                main_align="space_evenly", # Controls horizontal alignment
-                cross_align="center", # Controls vertical alignment
+                expanded=True,
+                main_align="space_evenly",
+                cross_align="center",
                 children = [
                     render.Image(src=BTC_ICON),
                     render.Text("$%d" % rate),
